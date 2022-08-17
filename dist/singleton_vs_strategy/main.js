d50b0c4501378a365336d19dcc1c945d140e2ee2
anirbanpranto <anirbanpranto@gmail.com>
2022-08-17 19:01:09 +0800

    fix : fixed benchmark implementation

--- dist/singleton_vs_strategy/main.js
+++ dist/singleton_vs_strategy/main.js
@@ -14,42 +14,46 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const singleton_1 = __importDefault(require("./singleton"));
 const strategy_1 = __importDefault(require("./strategy"));
+const perf_hooks_1 = require("perf_hooks");
+const start = perf_hooks_1.performance.now();
 const singleton_test = () => __awaiter(void 0, void 0, void 0, function* () {
     const funcs = [];
-    for (let i = 1; i <= 100000; i++) {
+    for (let i = 2; i <= 1000000; i++) {
         const func = singleton_1.default.runner("function1");
-        const isPrime = yield func(i);
+        const isPrime = func(i);
         funcs.push(isPrime);
     }
-    for (let i = 1; i <= 100000; i++) {
+    for (let i = 2; i <= 1000000; i++) {
         const func = singleton_1.default.runner("function2");
-        const isPrime = yield func(i);
+        const isPrime = func(i);
         funcs.push(isPrime);
     }
     const data = yield Promise.all(funcs);
-    console.log(data.length);
+    console.log(data);
     const used = process.memoryUsage();
     for (let key in used) {
         console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
     }
+    console.log("Time taken : ", perf_hooks_1.performance.now() - start);
 });
 const strategy_test = () => __awaiter(void 0, void 0, void 0, function* () {
     const funcs = [];
-    for (let i = 1; i <= 100000; i++) {
+    for (let i = 1; i <= 1000000; i++) {
         const strat_1 = new strategy_1.default("function1");
         const isPrime = strat_1.runner.prime(i);
         funcs.push(isPrime);
     }
-    for (let i = 1; i <= 100000; i++) {
+    for (let i = 1; i <= 1000000; i++) {
         const strat_2 = new strategy_1.default("function2");
         const isPrime = strat_2.runner.prime(i);
         funcs.push(isPrime);
     }
     const data = yield Promise.all(funcs);
-    console.log(data.length);
+    console.log(data);
     const used = process.memoryUsage();
     for (let key in used) {
         console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
     }
+    console.log("Time taken : ", perf_hooks_1.performance.now() - start);
 });
-singleton_test();
+strategy_test();
